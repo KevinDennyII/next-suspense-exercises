435915f35799d30d56af770d568283c5abb53018
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-07-07 22:35:52 -0500

    exercise two: instead of using loading.js we wrapped the appropriate component with React Suspense directly and use the Spinner component as a fallback as we wait for data to be loaded

--- src/app/02-interview/page.js
+++ src/app/02-interview/page.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { getComments } from '@/helpers/interview-helpers';
 import Comment from '@/components/Comment';
+import { Suspense } from 'react';
+import Spinner from "@/components/Spinner";
 
 export const dynamic = 'force-dynamic';
 
 async function InterviewExercise() {
-  const comments = await getComments();
 
   return (
     <>
@@ -126,15 +127,23 @@ async function InterviewExercise() {
 
       <section className="comments-section">
         <h2>Discussion</h2>
-        {comments.map((comment) => (
-          <Comment
-            key={comment.id}
-            comment={comment}
-          />
-        ))}
+        <Suspense fallback={<Spinner />}>
+        <Comments />
+          </ Suspense >
       </section>
     </>
   );
+}
+
+async function Comments(){
+  const comments = await getComments();
+
+  return comments.map((comment) => (
+      <Comment
+        key={comment.id}
+        comment={comment}
+      />
+  ))
 }
 
 export default InterviewExercise;
